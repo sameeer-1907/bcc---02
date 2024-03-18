1b59e808e19be3373ceb614b9dfc7f7fe25db99f
sameeer-1907 <142295169+sameeer-1907@users.noreply.github.com>
2024-03-18 22:42:16 +0530

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,139 @@
+function showPopup1() {
+  var popup = document.getElementById('popup1');
+  var overlay = document.getElementById('overlay');
+
+  // Display the overlay
+  overlay.style.display = 'block';
+
+  // Display the popup
+  popup.style.display = 'block';
+}
+
+// Function to close the popup and overlay
+function closePopup1() {
+  var popup = document.getElementById('popup1');
+  var overlay = document.getElementById('overlay');
+
+  // Hide the overlay
+  overlay.style.display = 'none';
+
+  // Hide the popup
+  popup.style.display = 'none';
+}
+
+function showPopup2() {
+  var popup = document.getElementById('popup2');
+  var overlay = document.getElementById('overlay');
+
+  // Display the overlay
+  overlay.style.display = 'block';
+
+  // Display the popup
+  popup.style.display = 'block';
+}
+
+// Function to close the popup and overlay
+function closePopup2() {
+  var popup = document.getElementById('popup2');
+  var overlay = document.getElementById('overlay');
+
+  // Hide the overlay
+  overlay.style.display = 'none';
+
+  // Hide the popup
+  popup.style.display = 'none';
+}
+
+function showPopup3() {
+  var popup = document.getElementById('popup3');
+  var overlay = document.getElementById('overlay');
+
+  // Display the overlay
+  overlay.style.display = 'block';
+
+  // Display the popup
+  popup.style.display = 'block';
+}
+
+// Function to close the popup and overlay
+function closePopup3() {
+  var popup = document.getElementById('popup3');
+  var overlay = document.getElementById('overlay');
+
+  // Hide the overlay
+  overlay.style.display = 'none';
+
+  // Hide the popup
+  popup.style.display = 'none';
+}
+
+function showPopup4() {
+  var popup = document.getElementById('popup4');
+  var overlay = document.getElementById('overlay');
+
+  // Display the overlay
+  overlay.style.display = 'block';
+
+  // Display the popup
+  popup.style.display = 'block';
+}
+
+// Function to close the popup and overlay
+function closePopup4() {
+  var popup = document.getElementById('popup4');
+  var overlay = document.getElementById('overlay');
+
+  // Hide the overlay
+  overlay.style.display = 'none';
+
+  // Hide the popup
+  popup.style.display = 'none';
+}
+
+function showPopup5() {
+  var popup = document.getElementById('popup5');
+  var overlay = document.getElementById('overlay');
+
+  // Display the overlay
+  overlay.style.display = 'block';
+
+  // Display the popup
+  popup.style.display = 'block';
+}
+
+// Function to close the popup and overlay
+function closePopup5() {
+  var popup = document.getElementById('popup5');
+  var overlay = document.getElementById('overlay');
+
+  // Hide the overlay
+  overlay.style.display = 'none';
+
+  // Hide the popup
+  popup.style.display = 'none';
+}
+
+function showPopup6() {
+  var popup = document.getElementById('popup6');
+  var overlay = document.getElementById('overlay');
+
+  // Display the overlay
+  overlay.style.display = 'block';
+
+  // Display the popup
+  popup.style.display = 'block';
+}
+
+// Function to close the popup and overlay
+function closePopup6() {
+  var popup = document.getElementById('popup6');
+  var overlay = document.getElementById('overlay');
+
+  // Hide the overlay
+  overlay.style.display = 'none';
+
+  // Hide the popup
+  popup.style.display = 'none';
+}
+
+
